bcb1dff537e3574b7df6ec628e126047df860f1e
ryota-sb <ryota.sb.1206@gmail.com>
2023-03-13 00:16:21 +0900

    hostsの設定

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -1,18 +1,14 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
 
-  # In the development environment your application's code is reloaded any time
-  # it changes. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  # Do not eager load code on boot.
   config.eager_load = false
 
-  # Show full error reports.
   config.consider_all_requests_local = true
+
+  config.hosts << "backend"
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
